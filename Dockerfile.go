c7ac634541a21bb1b7907dfc4c12c5fcb2ba792b
Dario Tranchitella <dario@tranchitella.eu>
2021-11-10 15:07:06 +0100

    feat: template-based build strategy using programming_language_runtime var

---
+++ Dockerfile.go
@@ -0,0 +1,17 @@
+FROM golang:1.17.3-alpine3.14
+WORKDIR /go/src/github.com/clastix/simple-app/
+#
+# go mod caching
+#
+COPY ["go.*", "."]
+RUN go mod download
+#
+# app compilation
+#
+COPY . .
+RUN CGO_ENABLED=0 GOOS=linux go build -v -o app
+
+FROM scratch
+COPY --from=0 /go/src/github.com/clastix/simple-app/app .
+EXPOSE 80
+ENTRYPOINT ["/app"]
